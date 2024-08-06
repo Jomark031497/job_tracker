a2a4d73b53417ab77b56e7b4882b2c08def63c2f
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-08-06 20:49:16 +0800

    refactor: fixed order of className

--- web/src/components/ui/Button.tsx
+++ web/src/components/ui/Button.tsx
@@ -12,11 +12,9 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ children, is
       ref={ref}
       {...rest}
       className={twMerge(
-        "rounded py-2 text-xs font-semibold transition-all md:text-sm",
-        rest.disabled
-          ? "border-gray-300 bg-gray-300 text-white hover:bg-gray-300"
-          : "bg-accent text-white hover:bg-primary",
+        "rounded bg-accent py-2 text-xs font-semibold text-white transition-all hover:bg-primary md:text-sm",
         rest.className,
+        rest.disabled ? "border-gray-300 bg-gray-300 text-white hover:bg-gray-300" : "",
       )}
     >
       {children}
